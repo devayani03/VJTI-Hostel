d49df79df363e2d63a4ee6dcea5ddec621756c1a
devayani03 <113435176+devayani03@users.noreply.github.com>
2022-12-16 14:11:51 +0530

    Update Home.js

--- vjtihostel02/src/components/pages/Home.js
+++ vjtihostel02/src/components/pages/Home.js
@@ -3,8 +3,10 @@ import '../../App.css';
 export default function Home(){
     return(
         <>
-        <h1 className='home' >VJTI Hostel</h1>
-       
+        <p className='home'>
+        <h1 className='par'>VJTI Hostel</h1>
+       <h2 className='par'>Established in 1990</h2> 
+        </p>
         </>
     );
 }
