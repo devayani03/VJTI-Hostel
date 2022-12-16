e96aee53bb14715bac0abf44e5ef16fc0950a8c3
devayani03 <113435176+devayani03@users.noreply.github.com>
2022-12-16 15:15:58 +0530

    Update ContactUs.js

--- vjtihostel02/src/components/pages/ContactUs.js
+++ vjtihostel02/src/components/pages/ContactUs.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function ContactUs(){
     return(
         <>
-        <h1 className='contactus'>CONTACT</h1>
+        <h1 className='contact'>contacts</h1>
+        
         </>
     );
 }
